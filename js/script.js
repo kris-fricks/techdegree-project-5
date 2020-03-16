d9f5df909854b81c258e1225d0c58a02911f1d6f
Kris Fricks <kris.fricks4@gmail.com>
2020-03-16 16:32:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@
 $(function() {       
     $("#search").on("keyup", function() {    //collects data from search id when key search is used
         let value = $(this).val().toLowerCase(); //converts anything typed into lowercase to match the captions
-        $("a").each(function() {                    //loops through a elements   
+        $("a").each(function() {                    //loops through 'a' elements   
             let str = $(this).attr("data-title");  //targets the search through the data-title attribute
             if (str.indexOf(value) > -1) {        //if anything typed matches a caption
-                $(this).show();                   //than show what caption matches
+                $(this).show();                   //then show what caption matches
             } else {                               //if not....
-                $(this).hide();                 //than hide what doesnt match
+                $(this).hide();                 //then hide what doesnt match
             }
         });
     });
